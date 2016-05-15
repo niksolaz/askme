f34ef2fed7b0440ccf1399040b1d418f07a74978
niksolaz <solazzo.nicola@gmail.com>
2016-05-15 20:41:55 +0200

    added Story and NewsFeed sub- component by Feeds component

--- src/components/shared/Feeds.jsx
+++ src/components/shared/Feeds.jsx
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import {Link} from 'react-router';
 import classnames from 'classnames';
 
+import NewsFeed from './NewsFeed';
+import Story from './Story';
+
 export default class Feeds extends Component {
   
   render() {
     return (
-      <div> Feeds </div>
+      <div>
+      	<NewsFeed />
+      	<Story />
+      </div>
     );
   }
 
