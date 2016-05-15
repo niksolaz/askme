5bd7a8bcaa8d0fedb56991489ccaac7b0d1f6960
niksolaz <solazzo.nicola@gmail.com>
2016-05-15 20:04:06 +0200

    added Feeds component

--- src/components/modules/Header.jsx
+++ src/components/modules/Header.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 
 import Navigation from '../shared/Navigation.jsx';
+import Feeds from '../shared/Feeds.jsx';
 
 export default class Header extends Component {
   render() {
     return (
       <div>
           <Navigation />
+          <Feeds />
       </div>
     );
   }
---
+++ src/components/shared/Feeds.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router';
+import classnames from 'classnames';
+
+export default class Feeds extends Component {
+  
+  render() {
+    return (
+      <div> Feeds </div>
+    );
+  }
+
+}
